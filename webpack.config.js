eaa4bc81b0f0214d3b9192d83fe50e9623ffe0f8
Masataka Okada <masataka.okada1117@gmail.com>
2020-10-10 12:44:29 +0900

    apply mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 
 const outputPath = path.resolve(__dirname, 'dist')
@@ -23,16 +24,9 @@ module.exports = {
         }
       },
       {
-        test: /\.css$/,
+        test: /\.(sc|c)ss$/,
         use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
+          MiniCssExtractPlugin.loader,
           'css-loader',
           'sass-loader'
         ]
@@ -60,6 +54,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: 'index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css'
     })
   ]
 }
